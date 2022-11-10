69e475adbcac21abbf5915d3fc99b85d7ceb0ce2
klausrahul <rahulmittal2805@gmail.com>
2022-11-10 16:52:56 +0530

    Coomiting client project

---
+++ insurence-client-cloud-config/src/test/java/com/rahul/spring/cloud/config/insurence/client/api/InsurenceClientCloudConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rahul.spring.cloud.config.insurence.client.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsurenceClientCloudConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
